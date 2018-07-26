0d13d4a2cd495a4a31bcb20fca7d9c762e54ff59
Briar Bowser <briarbear7@gmail.com>
2018-07-26 14:28:05 -0500

    Added accessories tab.

---
+++ lib/pages/home/accessories_tab.dart
@@ -0,0 +1,99 @@
+import 'dart:io';
+
+import 'package:built_collection/built_collection.dart';
+import 'package:dressr/app_state.dart';
+import 'package:dressr/models/accessory.dart';
+import 'package:dressr/pages/add_accessory.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+class AccessoryGridTile extends StatelessWidget {
+  AccessoryGridTile({this.accessory, this.onTap});
+
+  final Accessory accessory;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final imageFile = new File(accessory.image);
+
+    return new GridTile(
+      child: new Material(
+        child: new InkWell(
+          child: new Image.file(imageFile),
+          onTap: () => onTap(accessory)
+        )
+      )
+    );
+  }
+}
+
+class AccessoriesTab extends StatelessWidget {
+  const AccessoriesTab() : super();
+
+  void handleGridTileTapped(Accessory accessory) {
+
+  }
+
+  List<Widget> _buildGridTiles(BuildContext context, _AccessoriesTabViewModel viewModel) {
+    return viewModel.accessories
+    .map((accessory) => new AccessoryGridTile(accessory: accessory, onTap: handleGridTileTapped))
+    .toList();
+  }
+
+  Widget _buildBody(BuildContext context, _AccessoriesTabViewModel viewModel) {
+    if (viewModel.accessories.isEmpty)  {
+      return new Center(
+        child: const Text("Looks like it's time to add some accessories!"),
+      );
+    }
+
+    return new GridView.count(
+      crossAxisCount: 4,
+      children: _buildGridTiles(context, viewModel)
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new StoreConnector<AppState, _AccessoriesTabViewModel>(
+      distinct: true,
+      converter: (store) => new _AccessoriesTabViewModel.create(store),
+      builder: (context, viewModel) {
+        return new Scaffold(
+          body: _buildBody(context, viewModel),
+          floatingActionButton: new FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(context, new MaterialPageRoute(builder: (context) => new AddAccessoryModal()));
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _AccessoriesTabViewModel {
+  _AccessoriesTabViewModel({
+    this.accessories
+  });
+
+  final BuiltList<Accessory> accessories;
+
+  factory _AccessoriesTabViewModel.create(Store<AppState> store) {
+    return new _AccessoriesTabViewModel(
+      accessories: store.state.accessories
+    );
+  }
+
+  @override
+  operator ==(o) =>
+    identical(o, this) ||
+    o is _AccessoriesTabViewModel &&
+    accessories == o.accessories;
+
+  @override
+  int get hashCode => accessories.hashCode;
+}
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:dressr/pages/home/accessories_tab.dart';
 import 'package:dressr/pages/home/shirts_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -6,18 +7,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new DefaultTabController(
-      length: 1,
+      length: 2,
       child: new Scaffold(
         appBar: new AppBar(
           bottom: const TabBar(
             tabs: <Widget>[
               const Tab(icon: const Icon(FontAwesomeIcons.tshirt)),
+              const Tab(icon: const Icon(FontAwesomeIcons.blackTie)),
             ],
           ),
         ),
         body: const TabBarView(
           children: <Widget>[
-            const ShirtsTab()
+            const ShirtsTab(),
+            const AccessoriesTab(),
           ],
         ),
       )
